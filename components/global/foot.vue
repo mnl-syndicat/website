feaadd8cf0bdd73636f0d33a378ea11eb372d99d
Eban <account+github@eban.eu.org>
2024-04-28 17:18:08 +0200

    [🐛 Fix] Remove typo from footer

--- components/global/foot.vue
+++ components/global/foot.vue
@@ -6,7 +6,7 @@ const config = useRuntimeConfig();
   <section>
     <p>
       <Icon name="mdi:world"/> Site web réalisé par les bénévoles du MNL publié sous licence <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL-3.0</a>.
-      <br> <Icon name="mdi:git" /> Source code disponible sur <a href="https://github.com/ebanDev/mnl-website">GitHub</a>, commit actuel
+      <br> <Icon name="mdi:git" /> Code source disponible sur <a href="https://github.com/ebanDev/mnl-website">GitHub</a>, commit actuel
       <a :href="'https://github.com/ebanDev/mnl-website/commit/' + config.public.COMMIT_SHA">{{ config.public.COMMIT_SHA.slice(0, 7) }}</a>.
     </p>
   </section>

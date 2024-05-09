95bf0628b84a22920ba2221d1daf732b9a1cab9a
Eban <contact@eban.eu.org>
2024-05-09 17:00:31 +0200

    [✨ Improvement] Open partners links in new tab

--- components/index/partners.vue
+++ components/index/partners.vue
@@ -2,7 +2,7 @@
   <section>
     <h1>PARTENAIRES</h1>
     <div class="icon-list">
-      <a :href="partner.link" class="icon-list-el" v-for="partner in getPartners()">
+      <a :href="partner.link" class="icon-list-el" v-for="partner in getPartners()" target="_blank">
         <NuxtImg width="70" height="70" :src="partner.icon" alt="" />
         <p>{{ partner.name }}</p>
       </a>
